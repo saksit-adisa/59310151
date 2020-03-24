483b26a0b66f718949399540167dfa6a9477b2f7
saksit-adisa <saksit-adisa>
2020-03-24 11:01:16 +0700

    teu3

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <H1>
 <?php echo "รหัสนิสิต-59310151"; ?><br>
 <?php echo "ชื่อ-สกุล นาย ศักดิ์สิทธิ์ อดิษะ"; ?><br>
-<iframe width="500" height="500"
-src="https://youtu.be/RXRm11Tyg84[/clip]">
-</iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/RXRm11Tyg84" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </BODY>
 </HTML>

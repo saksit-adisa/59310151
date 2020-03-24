a6bd8e2564668705b5b7ad75a785655d70f3ce8d
saksit-adisa <saksit-adisa>
2020-03-24 10:26:12 +0700

    tr1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <HEAD><TITLE> My Homepage </TITLE></HEAD>
 <BODY BGCOLOR=#FFFFFF>
 <H1>
-<?php echo "รหัสนิสิต-62310151"; ?><br>
+<?php echo "รหัสนิสิต-59310151"; ?><br>
 <?php echo "ชื่อ-สกุล นาย ศักดิ์สิทธิ์ อดิษะ"; ?><br>
 <iframe width="500" height="500"
 src="https://youtu.be/RXRm11Tyg84">
